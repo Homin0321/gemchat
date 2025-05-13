931c0ebd7ebb0372a72a2f201bb3a861228e180a
Homin0321 <167429242+Homin0321@users.noreply.github.com>
2025-05-14 08:53:07 +0900

    model update

--- gemchat.py
+++ gemchat.py
@@ -58,7 +58,8 @@ with st.sidebar:
     model_options = [
         "gemini-2.0-flash",
         "gemini-2.0-flash-lite",
-        "gemini-2.5-pro-exp-03-25"
+        "gemini-2.5-flash-preview-04-17",
+        "gemini-2.5-pro-preview-05-06"
     ]
 
     selected_model = st.selectbox(
