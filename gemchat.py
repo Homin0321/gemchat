803f7d1477739d9bc17375699237974f84b04050
Homin0321 <167429242+Homin0321@users.noreply.github.com>
2025-08-13 20:38:11 +0900

    Update gemchat.py

--- gemchat.py
+++ gemchat.py
@@ -81,6 +81,7 @@ with st.sidebar:
     model_options = [
         "gemini-2.5-flash-lite",
         "gemini-2.5-flash",
+        "gemini-2.5-pro",
         "gemini-2.0-flash-lite",
         "gemini-2.0-flash",
     ]
